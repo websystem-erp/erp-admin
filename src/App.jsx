ce161c24f7031c7157427802d7beabc507a6d8a4
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-05-21 16:05:41 +0530

    A user's session will end after one hour.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LogIn from "./LogIn";
 import Layout from "./Layout";
@@ -6,13 +6,39 @@ import "./App.css";
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const timeoutRef = useRef(null);
+
+	const logout = () => {
+		setIsLoggedIn(false);
+		sessionStorage.removeItem("isLoggedIn");
+	};
+
+	const resetTimeout = () => {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
+		}
+		timeoutRef.current = setTimeout(logout, 3600000);
+	};
 
 	useEffect(() => {
-		// Check if user is logged in when the component mounts
 		const storedIsLoggedIn = sessionStorage.getItem("isLoggedIn");
 		if (storedIsLoggedIn === "true") {
 			setIsLoggedIn(true);
 		}
+		resetTimeout();
+
+		const handleActivity = () => {
+			resetTimeout();
+		};
+
+		window.addEventListener("mousemove", handleActivity);
+		window.addEventListener("keypress", handleActivity);
+
+		return () => {
+			clearTimeout(timeoutRef.current);
+			window.removeEventListener("mousemove", handleActivity);
+			window.removeEventListener("keypress", handleActivity);
+		};
 	}, []);
 
 	return (
