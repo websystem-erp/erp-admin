d4ee6935bebbb7ce58998c5e523fb94cd06c0256
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-05-27 18:53:51 +0530

    forgot password functionality added

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { jwtDecode } from "jwt-decode"; // Correct import with curly brackets
+import { jwtDecode } from "jwt-decode";
+import API_ENDPOINTS from "./API/apiEndpoints";
 
 const LogIn = ({ setIsLoggedIn, setToken, setUserData }) => {
 	const [credentials, setCredentials] = useState({ email: "", password: "" });
 	const [errorMessage, setErrorMessage] = useState("");
+	const [forgotPasswordEmail, setForgotPasswordEmail] = useState("");
+	const [showForgotPassword, setShowForgotPassword] = useState(false);
+	const [forgotPasswordMessage, setForgotPasswordMessage] = useState("");
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -33,10 +37,7 @@ const LogIn = ({ setIsLoggedIn, setToken, setUserData }) => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const response = await axios.post(
-				"https://erp-system-backend.onrender.com/api/v1/admin/login",
-				credentials
-			);
+			const response = await axios.post(API_ENDPOINTS.ADMIN_LOGIN, credentials);
 			if (response.data.success) {
 				setIsLoggedIn(true);
 				setToken(response.data.token);
@@ -54,56 +55,121 @@ const LogIn = ({ setIsLoggedIn, setToken, setUserData }) => {
 		}
 	};
 
+	const handleForgotPassword = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post(API_ENDPOINTS.ADMIN_FORGOT_PASSWORD, {
+				email: forgotPasswordEmail,
+			});
+			if (response.data.success) {
+				setForgotPasswordMessage("Password reset link sent to your email.");
+			}
+		} catch (error) {
+			setForgotPasswordMessage(
+				"An error occurred. Please try again later or contact support."
+			);
+		}
+	};
+
 	return (
 		<div className="flex items-center h-screen w-full">
 			<div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-				<span className="block w-full text-xl uppercase font-bold mb-4">
-					Login
-				</span>
-				<form className="mb-4" onSubmit={handleSubmit}>
-					<div className="mb-4 md:w-full">
-						<label htmlFor="email" className="block text-xs mb-1">
-							Username or Email
-						</label>
-						<input
-							className="w-full border rounded p-2 outline-none focus:shadow-outline"
-							type="text"
-							name="email"
-							id="email"
-							placeholder="Username or Email"
-							value={credentials.email}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className="mb-6 md:w-full">
-						<label htmlFor="password" className="block text-xs mb-1">
-							Password
-						</label>
-						<input
-							className="w-full border rounded p-2 outline-none focus:shadow-outline"
-							type="password"
-							name="password"
-							id="password"
-							placeholder="Password"
-							value={credentials.password}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className="flex flex-col gap-4 ">
-						<Link to="" className="cursor-pointer text-sm text-right">
-							Forgot Password ?
-						</Link>
+				{showForgotPassword ? (
+					<>
+						<h2 className="block w-full text-xl uppercase font-bold mb-4">
+							Forgot Password
+						</h2>
+						<form onSubmit={handleForgotPassword}>
+							<div className="mb-4 md:w-full">
+								<label
+									htmlFor="forgotPasswordEmail"
+									className="block text-xs mb-1"
+								>
+									Email
+								</label>
+								<input
+									className="w-full border rounded p-2 outline-none focus:shadow-outline"
+									type="email"
+									name="forgotPasswordEmail"
+									id="forgotPasswordEmail"
+									placeholder="Email"
+									value={forgotPasswordEmail}
+									onChange={(e) => setForgotPasswordEmail(e.target.value)}
+								/>
+							</div>
+							<button
+								type="submit"
+								className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
+							>
+								Send Reset Link
+							</button>
+							{forgotPasswordMessage && (
+								<p className="text-red-500 text-xs mt-2">
+									{forgotPasswordMessage}
+								</p>
+							)}
+						</form>
 						<button
-							type="submit"
-							className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
+							className="text-blue-500 text-xs mt-2"
+							onClick={() => setShowForgotPassword(false)}
 						>
-							Login
+							Back to Login
 						</button>
-					</div>
-					{errorMessage && (
-						<p className="text-red-500 text-xs mt-2">{errorMessage}</p>
-					)}
-				</form>
+					</>
+				) : (
+					<>
+						<h2 className="block w-full text-xl uppercase font-bold mb-4">
+							Login
+						</h2>
+						<form className="mb-4" onSubmit={handleSubmit}>
+							<div className="mb-4 md:w-full">
+								<label htmlFor="email" className="block text-xs mb-1">
+									Username or Email
+								</label>
+								<input
+									className="w-full border rounded p-2 outline-none focus:shadow-outline"
+									type="text"
+									name="email"
+									id="email"
+									placeholder="Username or Email"
+									value={credentials.email}
+									onChange={handleChange}
+								/>
+							</div>
+							<div className="mb-6 md:w-full">
+								<label htmlFor="password" className="block text-xs mb-1">
+									Password
+								</label>
+								<input
+									className="w-full border rounded p-2 outline-none focus:shadow-outline"
+									type="password"
+									name="password"
+									id="password"
+									placeholder="Password"
+									value={credentials.password}
+									onChange={handleChange}
+								/>
+							</div>
+							<div className="flex flex-col gap-4 ">
+								<a
+									className="cursor-pointer text-sm text-right"
+									onClick={() => setShowForgotPassword(true)}
+								>
+									Forgot Password ?
+								</a>
+								<button
+									type="submit"
+									className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
+								>
+									Login
+								</button>
+							</div>
+							{errorMessage && (
+								<p className="text-red-500 text-xs mt-2">{errorMessage}</p>
+							)}
+						</form>
+					</>
+				)}
 			</div>
 		</div>
 	);
